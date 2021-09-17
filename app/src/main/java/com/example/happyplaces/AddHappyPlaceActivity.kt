8f3c2d3b971c9805b3674eb8be7896fcb5685b9e
daniel shelton <danielshelton90@gmail.com>
2021-09-16 22:58:02 -0700

    updated permissions for camera/file storage access

--- app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
@@ -1,9 +1,21 @@
 package com.example.happyplaces
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
+import android.widget.Toast
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kotlinx.android.synthetic.main.activity_add_happy_place.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -30,19 +42,83 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
             updateDateInView()
         }
         et_date.setOnClickListener(this)
+        tv_add_image.setOnClickListener(this)
     }
 
-    override fun onClick(view: View?) {
-        when(view!!.id) {
+    override fun onClick(v: View?) {
+        when (v!!.id) {
             R.id.et_date -> {
                 DatePickerDialog(
                     this@AddHappyPlaceActivity,
-                    dateSetListener,
-                    cal.get(Calendar.YEAR),
+                    dateSetListener, // This is the variable which have created globally and initialized in setupUI method.
+                    // set DatePickerDialog to point to today's date when it loads up
+                    cal.get(Calendar.YEAR), // Here the cal instance is created globally and used everywhere in the class where it is required.
                     cal.get(Calendar.MONTH),
-                    cal.get(Calendar.DAY_OF_MONTH)).show()
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
             }
+
+            // TODO(Step 2 : Adding an alert dialog for selection of image.)
+            // START
+            R.id.tv_add_image -> {
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogItems =
+                    arrayOf("Select photo from gallery", "Capture photo from camera")
+                pictureDialog.setItems(
+                    pictureDialogItems
+                ) { dialog, which ->
+                    when (which) {
+                        // Here we have create the methods for image selection from GALLERY
+                        0 -> choosePhotoFromGallery()
+                        1 -> Toast.makeText(this@AddHappyPlaceActivity,"Camera selection coming soon...", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                pictureDialog.show()
+            }
+            // END
         }
+    }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withActivity(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    // Here after all the permission are granted launch the gallery to select and image.
+                    if (report!!.areAllPermissionsGranted()) {
+                        Toast.makeText(this@AddHappyPlaceActivity,"Storage READ/WRITE permission are granted. Now you can select an image from GALLERY or lets says phone storage.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+            }).onSameThread()
+            .check()
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It looks like you have turned off permissions required for this feature. It can be enabled under Application settings.")
+            .setPositiveButton("GO TO SETTINGS") {
+                _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }.setNegativeButton("Cancel"){dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 
     private fun updateDateInView() {
