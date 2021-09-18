4ee7fb4e80b21688438f83d646c9c6d1a96046e8
daniel shelton <danielshelton90@gmail.com>
2021-09-18 16:59:44 -0700

    reads list of HappyPlaces from db

--- app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.happyplaces.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.happyplaces.R
+import com.example.happyplaces.models.HappyPlaceModel
+import com.happyplaces.database.DatabaseHandler
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +18,19 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AddHappyPlaceActivity::class.java)
             startActivity(intent)
         }
+        getHappyPlacesListFromLocalDB()
+    }
+
+    private fun getHappyPlacesListFromLocalDB() {
+        val dbHandler = DatabaseHandler(this)
+        val getHappyPlaceList : ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
+
+        if (getHappyPlaceList.isNotEmpty()) {
+            for (i in getHappyPlaceList) {
+                Log.i("Title", i.title)
+                Log.i("Description", i.description)
+            }
+        }
+
     }
 }
